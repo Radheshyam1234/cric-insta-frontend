49c01bff249ab97eef71bb3703b54bb63dd319a2
Satyam chandra <satyamchandra7277@gmail.com>
2023-12-26 02:08:26 +0530

    fixed the input boxes size

--- src/components/input/password-input.jsx
+++ src/components/input/password-input.jsx
@@ -5,11 +5,7 @@ const PasswordInput = ({ label, labelClassName, containerClassName, inputClassNa
       <div className={`${inputContainerClassName}`}>
         <input
           type='password'
-<<<<<<< Updated upstream
-          className={`px-5 h-full w-full outline-none border-none h-[44px] bg-[#393f5c] text-[#FFF] rounded-[2px] ${inputClassName} `}
-=======
-          className={`px-5 w-full outline-none border-none h-11 bg-[#393f5c] text-[#FFF] rounded-[2px] ${inputClassName} `}
->>>>>>> Stashed changes
+          className={`px-5 h-11 w-full outline-none border-none  bg-[#393f5c] text-[#FFF] rounded-[2px] ${inputClassName} `}
           placeholder={placeHolder}
           required={required}
           disabled={disabled}
